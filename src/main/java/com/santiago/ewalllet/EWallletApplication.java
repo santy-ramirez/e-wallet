b875fc67dfc6408a5ba39044b872e936f9d2e208
santiago <santysjr956@gmail.com>
2023-03-18 17:12:00 -0300

    project base

---
+++ src/main/java/com/santiago/ewalllet/EWallletApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.ewalllet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWallletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWallletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santiago/ewalllet/EWallletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.ewalllet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWallletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
